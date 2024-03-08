704d7cae4503c6231614965af790c80a6bb23dca
wwelch <wwelch@local.domain>
2024-03-07 22:55:02 -0800

    RES-4 cleanup imports in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {RolesModule} from "../roles/roles.module";
 
 const routes: Routes = [
 	{
