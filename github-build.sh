217691a46808e0889f7c3458d92d9f3854f45069
wwelch <wwelch@local.domain>
2024-03-08 12:04:36 -0800

    RES-7 add 404 page to get around routing issues

--- github-build.sh
+++ github-build.sh
@@ -1,1 +1,1 @@
-rm -rf docs && ng build --output-path docs --base-href /anthropic-talent-tracker/ && cp -r docs/browser/ docs/ && rm -rf docs/browser
+rm -rf docs && ng build --output-path docs --base-href /anthropic-talent-tracker/ && cp -r docs/browser/ docs/ && rm -rf docs/browser && cp docs/index.html docs/404.html
